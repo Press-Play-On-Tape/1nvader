9d68d842137e2246fe83989185b2a7aa99e1003f
Filmote <simon@bloggingadeadhorse.com>
2022-05-22 12:55:48 +1000

    Update EEPROM_Utils.h

--- Invader/src/utils/EEPROM_Utils.h
+++ Invader/src/utils/EEPROM_Utils.h
@@ -12,6 +12,8 @@
 #define EEPROM_ROTATION             EEPROM_START + 9
 #define EEPROM_LOCK_LVL_0           EEPROM_START + 10
 #define EEPROM_LOCK_LVL_1           EEPROM_START + 11
+#define EEPROM_END                  EEPROM_START + 12
+#define EEPROM_CHECKSUM             EEPROM_END
 
 class EEPROM_Utils {
 
@@ -28,6 +30,7 @@ class EEPROM_Utils {
         static void saveRotation(GameRotation rotation);
         static uint8_t getLevel(uint8_t level);
         static void saveLevel(uint8_t level, uint8_t value);
+        static int16_t checkSum(bool update);
 
 };
 
@@ -48,7 +51,7 @@ void EEPROM_Utils::initEEPROM(bool force) {
     byte c2 = EEPROM.read(EEPROM_START_C2);
 
     if (c1 != letter1 || c2 != letter2 || force) { 
-
+Serial.println("a");
         uint16_t hs = 0;
         uint8_t mode = static_cast<uint8_t>(GameMode::Single);
         uint8_t rotation = static_cast<uint8_t>(GameRotation::Portrait);
@@ -63,7 +66,23 @@ void EEPROM_Utils::initEEPROM(bool force) {
         EEPROM.put(EEPROM_LOCK_LVL_0, 0);
         EEPROM.put(EEPROM_LOCK_LVL_1, 0);
 
+        EEPROM_Utils::checkSum(true);
+
     }
+    else {
+Serial.println("b");
+
+        int16_t checkSumOld = 0;
+        int16_t checkSumNow = EEPROM_Utils::checkSum(false);
+        EEPROM.get(EEPROM_CHECKSUM, checkSumOld);
+
+        if (checkSumNow != checkSumOld) {
+
+            //EEPROM_Utils::initEEPROM(true);
+
+        }
+        
+    }    
 
 }
 
@@ -93,6 +112,8 @@ void EEPROM_Utils::saveScore(GameMode mode, uint16_t score) {
 
     }
 
+    EEPROM_Utils::checkSum(true);
+
 }
 
 
@@ -114,6 +135,7 @@ GameMode EEPROM_Utils::getMode() {
 void EEPROM_Utils::saveMode(GameMode mode) {
 
     EEPROM.update(EEPROM_MODE, static_cast<uint8_t>(mode));
+    EEPROM_Utils::checkSum(true);
 
 }
 
@@ -136,6 +158,7 @@ GameRotation EEPROM_Utils::getRotation() {
 void EEPROM_Utils::saveRotation(GameRotation rotation) {
 
     EEPROM.update(EEPROM_ROTATION, static_cast<uint8_t>(rotation));
+    EEPROM_Utils::checkSum(true);
 
 }
 
@@ -157,5 +180,31 @@ uint8_t EEPROM_Utils::getLevel(uint8_t level) {
 void EEPROM_Utils::saveLevel(uint8_t level, uint8_t value) {
 
     EEPROM.update(EEPROM_LOCK_LVL_0 + level, value);
+    EEPROM_Utils::checkSum(true);
+
+}
+
+
+/* -----------------------------------------------------------------------------
+ *   Generate and optionally save a check sum .. 
+ */
+int16_t EEPROM_Utils::checkSum(bool update) {
+
+    int16_t checksum = 0;
+
+    for (uint8_t i = 0; i < (EEPROM_END - EEPROM_START); i++) {
+
+        checksum = checksum + ((i % 2 == 0 ? 1 : -1) * eeprom_read_byte(reinterpret_cast<uint8_t *>(EEPROM_START + i)));
+
+    }
+
+    if (update) {
+Serial.println("c");        
+
+        EEPROM.put(EEPROM_CHECKSUM, checksum);
+    }
+
+Serial.println("d");        
+    return checksum;
 
 }
